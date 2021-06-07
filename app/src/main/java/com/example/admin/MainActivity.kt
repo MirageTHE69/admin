4cc50714d57790c1af52a4461d005821dff56588
jeet2610 <jeetshah0@icloud.com>
2021-06-08 00:42:11 +0530

    First Commit by Jeet for adding Bottom Navigation

--- app/src/main/java/com/example/admin/MainActivity.kt
+++ app/src/main/java/com/example/admin/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomAppBar: BottomAppBar;
+    lateinit var bottomNavigationView: BottomNavigationView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView =findViewById(R.id.bottomNavigationView)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment;
+        NavigationUI.setupWithNavController(bottomNavigationView,
+            navHostFragment.navController);
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(1).isEnabled = true
+
+        navController.addOnDestinationChangedListener {
+                _, destination, _ ->
+            when (destination.id) {
+                R.id.orderList -> {
+                    showBottomNav()
+
+                    hideTitleBar()
+                }
+                R.id.product_List -> {
+                    showBottomNav()
+                    hideTitleBar()
+                }
+                R.id.orderstatus -> {
+                    hideTitleBar()
+                    showBottomNav()
+                }
+
+
+            }
+        }
+
     }
+    private fun hideTitleBar() {
+        this.supportActionBar?.hide()
+    }
+
+
+
+    private fun showBottomNav() {
+        bottomNavigationView.visibility = View.VISIBLE
+        bottomAppBar.visibility = View.VISIBLE
+
+
+    }
+
 }
+
--- app/src/main/java/com/example/admin/OrderList.kt
+++ app/src/main/java/com/example/admin/OrderList.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.admin.ui.adpater.OrderAdpater
@@ -12,8 +13,9 @@ import com.example.admin.ui.adpater.OrderAdpater
 
 class OrderList : Fragment() {
 
-    private lateinit var adapter: OrderAdpater;
+    lateinit var adapter: OrderAdpater;
     lateinit var recyclerView: RecyclerView;
+
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +28,22 @@ class OrderList : Fragment() {
        // return inflater.inflate(R.layout.fragment_order_list, container, false)
         val view = inflater.inflate(R.layout.fragment_order_list, container, false)
         recyclerView = view.findViewById<RecyclerView>(R.id.rcview)
-        recyclerView.adapter = adapter;
+        recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(activity)
 
         return  view
 
+    }
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening();
+    }
+
+
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 
 
